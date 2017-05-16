e005c935c8c17f25d2c2700c3370730b4e9f49e7
AnnaVinnik <vinnik.anna@rambler.ru>
2017-05-16 15:30:05 +0700

    Add print

--- lib.c
+++ lib.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define CSI "\x1B\x5B" 
 #define HASH_MUL 31
 #define HASH_SIZE 71
 
@@ -17,3 +18,29 @@ int j;
     return h%HASH_SIZE;
 }
 
+
+void print(char *string, int position, int sizeSearch, int sizeString)
+{
+char colors[][5] = {
+ "0;30", /* Black */ "1;30", /* Dark Gray */
+ "0;31", /* Red */ "1;31", /* Bold Red */
+ "0;32", /* Green */ "1;32", /* Bold Green */
+ "0;33", /* Yellow */ "1;33", /* Bold Yellow */
+ "0;34", /* Blue */ "1;34", /* Bold Blue */
+ "0;35", /* Purple */ "1;35", /* Bold Purple */
+ "0;36", /* Cyan */ "1;36" /*Bold Cyan */ };
+int i;	
+
+    for (i = 0; i < position; i++){
+        printf("%c", string[i]);
+    }
+    printf("%s%sm",CSI,colors[9]);
+    for (i = position; i < (position + sizeSearch); i++){
+        printf("%c", string[i]);
+    }
+    printf("%s0m",CSI);
+    for (i = position + sizeSearch; i < sizeString; i++){
+        printf("%c", string[i]);
+    }
+    printf("\n");
+}
--- lib.h
+++ lib.h
@@ -2,5 +2,6 @@
 #define LIB_H
 
 int hash_kp(char *key, int i);
+void print(char *string, int position, int sizeSearch, int sizeString);
 
 #endif
