06e7c1ee60359057e73dae480d22d9c53322c679
AnnaVinnik <vinnik.anna@rambler.ru>
2017-05-16 10:20:26 +0700

    Adding strcmp

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 #define CSI "\x1B\x5B" 
 
 int main(){
 FILE *f, *f2;
-char a[10];
-char g[20];
+char search[10] = "my";
+char str[20][10] = {"Hello", "my", "frend"};
 int b[10][5];
 int d = 8;
-int i;
+int i, k = 0;
 char colors[][5] = {
  "0;30", /* Black */ "1;30", /* Dark Gray */
  "0;31", /* Red */ "1;31", /* Bold Red */
@@ -26,7 +27,7 @@ b[0][0] = 2;
 	//b = fgetc(f);
 
 		//fscanf(f, "%s", a);
-		printf("%d\n", b[0][0]);
+		//printf("%d\n", b[0][0]);
 
 	//f2 = fopen("file2.txt", "a");
 		//fprintf(f2, "%s", g);
@@ -40,6 +41,32 @@ b[0][0] = 2;
 printf("Hello\n");*/ 
 	//fclose(f);
 	//fclose(f2);
+
+/*str[0] = "Hello";
+str[1] = "my";
+str[2] = "frend";*/
+i = 0;
+     for (i = 0; i < 3; i++){
+         if (strcmp(search, str[i]) == 0)
+             k = i;
+     }
+     for (i = 0; i < k; i++)
+         printf("%s ", str[i]);
+     printf("%s%sm",CSI,colors[9]);
+     printf("%s ", str[k]);
+     printf("%s0m",CSI);
+     for (i = k + 1; i < 5; i++)
+         printf("%s ", str[i]);
+     printf("\n");
+      
+    // for (i = 0; i < k; i++)
+      //   printf("%s ",str[i]);
+     //printf("%s%sm",CSI,colors[13]);
+     //printf("%s ", str[k]);
+     //for (i = k + 1; i < 5; i++)
+         //printf("%s ", str[i]);
+     //printf("\n"); 
+
 
 	return 0;
 
